1a5f891873667ecc6d6a2a6df74051f4cc3c1b15
Ripassorker <ripassorker02@glail.com>
2022-11-21 04:36:32 +0600

    implement payment system

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 const app = express();
 
 app.use(cors());
@@ -17,25 +19,6 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorization access !!" });
-  }
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.USER_TOKEN, function (err, decoded) {
-    if (err) {
-      return res.status(402).send({
-        success: false,
-        message: "Forbidden access",
-      });
-    }
-    req.decoded = decoded;
-    next();
-  });
-}
-
 async function run() {
   try {
     const appointmentOptionCollection = client
@@ -47,6 +30,30 @@ async function run() {
       .collection("bokkings");
     const usersCollections = client.db("doctor-portal").collection("users");
     const doctorCollections = client.db("doctor-portal").collection("doctors");
+    const paymentsCollections = client
+      .db("doctor-portal")
+      .collection("payments");
+
+    //.............verify JWT........................
+
+    function verifyJWT(req, res, next) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorization access !!" });
+      }
+      const token = authHeader.split(" ")[1];
+
+      jwt.verify(token, process.env.USER_TOKEN, function (err, decoded) {
+        if (err) {
+          return res.status(402).send({
+            success: false,
+            message: "Forbidden access",
+          });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    }
 
     //.............verify Admin........................
 
@@ -74,11 +81,29 @@ async function run() {
       const user = await usersCollections.findOne(filter);
       if (user) {
         const token = jwt.sign({ email }, process.env.USER_TOKEN, {
-          expiresIn: "1d",
+          expiresIn: "7d",
         });
         return res.send({ token });
       }
       res.status(401).send({ token: "" });
+    });
+
+    //....................payment .......................
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const price = req.body.price;
+      const amount = price * 100;
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     //   -----------appointmentOption------------------
@@ -100,7 +125,6 @@ async function run() {
         );
         option.slots = remaining;
       });
-
       res.send(options);
     });
 
@@ -116,7 +140,6 @@ async function run() {
 
     app.get("/bookings", verifyJWT, async (req, res) => {
       const email = req.query.email;
-
       const decodedEmail = req.decoded.email;
       if (decodedEmail !== email) {
         res.status(403).send({
@@ -127,6 +150,13 @@ async function run() {
         email: email,
       };
       const result = await bookingCollections.find(filter).toArray();
+      res.send(result);
+    });
+
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await bookingCollections.findOne(filter);
       res.send(result);
     });
 
@@ -156,12 +186,16 @@ async function run() {
       res.send(result);
     });
 
+    //..................admin  verify kora hocce user ta admin ki na.........................
+
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
       const user = await usersCollections.findOne(filter);
       res.send({ isAdmin: user?.role === "Admin" });
     });
+
+    //................user to admin ....................
 
     app.put("/users/admin/:id", verifyJWT, verifyAdmin, async (req, res) => {
       const id = req.params.id;
@@ -179,6 +213,24 @@ async function run() {
       );
       res.send(result);
     });
+
+    //.............temporary..................
+
+    // app.get("/addPrice", async (req, res) => {
+    //   const query = {};
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $set: {
+    //       price: 87,
+    //     },
+    //   };
+    //   const result = await appointmentOptionCollection.updateMany(
+    //     query,
+    //     updateDoc,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -208,6 +260,26 @@ async function run() {
     app.post("/dashboard/doctors", verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = req.body;
       const result = await doctorCollections.insertOne(doctor);
+      res.send(result);
+    });
+
+    //............ payments ...............
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollections.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingCollections.updateOne(
+        filter,
+        updatedDoc
+      );
       res.send(result);
     });
   } finally {
